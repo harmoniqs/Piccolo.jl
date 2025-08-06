916903e513e8f896ee93a0541560cfaee8570b04
Jack Champagne <jackchampagne.r@gmail.com>
2025-08-05 22:30:30 -0400

    add features

---
+++ docs/literate/features.jl
@@ -0,0 +1,46 @@
+#=
+
+# Feature Listing
+
+# ## Quantum Systems
+# ### Some Basic Systems
+# ### Time-dependent Systems
+# ### Open Systems
+# ### Composite systems
+
+# ## Basic Quantum System operations
+# ### Internal representations: isomorphisms (state, operator, density matrices, dynamics, etc.
+# ### Drift vs. drive
+# ### Rollouts & fidelities
+# ### Reachability tests
+# ### Direct sums
+# ### GRAPE
+
+# ## Trajectory Initialization
+
+# ## Hamiltonian construction + limitations
+
+# ## Optimization Parameters
+# ### Custom Objectives
+# ### Custom Constraints
+
+# ## Optimization solvers
+# ### TODO: IPOPT vs. MadNLP
+# ### TODO: Custom callbacks, trajectory checkpointing, etc.
+
+# ## Problem Templates usage
+# ### Min time
+# ### smooth 
+# ### …
+# ### State vs Unitary problems
+
+# ## Global Parameters
+# ## Callbacks
+# ## Rollouts
+# ## Fidelities
+# ## Subspaces/leakage
+# ## Integrators 
+# ## Plotting
+
+=#
+
