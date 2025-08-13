f22eef372ded7dfad8167a4c56a8bfba8a22b019
Jack Champagne <jackchampagne.r@gmail.com>
2025-08-12 23:06:42 -0400

    add self-hosted to test workflow speed

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,10 @@ using PiccoloDocsTemplate
 
 pages = [
     "Home" => "index.md",
-    # "Quickstart" => "generated/quickstart.md",
+    "Quickstart" => "generated/quickstart.md",
     "Features" => "generated/features.md",
     "Other Considerations" => "generated/other.md",
-    # "Examples" => ["generated/multilevel_transmon.md"],
+    "Examples" => ["generated/multilevel_transmon.md"],
     "Contribution Guide" => "contribution_guide.md",
     "Release Notes" => "release_notes.md",
 ]
